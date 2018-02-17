041f969ab0a0e18bb37fae3c2ce635e5ca3d3536
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-02-17 17:42:01 -0600

    connected each array by ID for a search term

--- arraymethods.js
+++ arraymethods.js
@@ -1,107 +1,140 @@
-const songs = [
+var songs = [
     {
-        song: "Jump (For My Love)"
+        song: "Jump (For My Love)",
+        id: 1
+        
     },
     {
-        song: "Foolish"
+        song: "Foolish",
+        id: 2
     },
     {
-        song: "I Will Follow You Into the Dark"
+        song: "I Will Follow You Into the Dark",
+        id: 3
     },
     {
-        song: "'Cause I'm A Man"
+        song: "'Cause I'm A Man",
+        id: 4
     },
     {
-        song: "Gloria"
+        song: "Gloria",
+        id: 5
     },
     {
-        song: "One Hour Ahead of the Posse"
+        song: "One Hour Ahead of the Posse",
+        id: 6
     },
     {
-        song: "I Can Change"
+        song: "I Can Change",
+        id: 7
     },
     {
-        song: "Redbone"
+        song: "Redbone",
+        id: 8
     },
     {
-        song: "Sad Songs (Say So Much)"
+        song: "Sad Songs (Say So Much)",
+        id: 9
     },
     {
-        song: "Don't Dream It's Over"
+        song: "Don't Dream It's Over",
+        id: 10
     }
 
-], 
-    albums = [
+], albums = [
     {
-        album: "Break Out (1984 Version)"
+        album: "Break Out (1984 Version)",
+        id: 1
     },
     {
-        album: "Concrete Rose"
+        album: "Concrete Rose",
+        id: 2
     },
     {
-        album: "Plans"
+        album: "Plans",
+        id: 3
     },
     {
-        album: "Currents"
+        album: "This is Happening",
+        id: 4
     },
     {
-        album: "The Best of Branigan"
+        album: "Currents",
+        id: 5
     },
     {
-        album: "Burl Ives Greatest Hits"
+        album: "The Best of Branigan",
+        id: 6
     },
     {
-        album: "Awaken, My Love!"
+        album: "Burl Ives Greatest Hits",
+        id: 7
     },
     {
-        album: "Breaking Hearts"
+        album: "Awaken, My Love!",
+        id: 8
     },
     {
-        album: "Crowded House"
-    },
-], 
-    artists = [
-    {
-        band: "The Pointer Sisters"
+        album: "Breaking Hearts",
+        id: 9
     },
     {
-        band: "Ashanti"
+        album: "Crowded House",
+        id: 10
+    },
+], artists = [
+    {
+        band: "The Pointer Sisters",
+        id: 1
     },
     {
-        band: "Death Cab for Cutie"
+        band: "Ashanti",
+        id: 2
     },
     {
-        band: "LCD Soundsystem"
+        band: "Death Cab for Cutie",
+        id: 3
     },
     {
-        band: "Tame Impala"
+        band: "LCD Soundsystem",
+        id: 4
     },
     {
-        band: "Gloria Branigan"
+        band: "Tame Impala",
+        id: 5
     },
     {
-        band: "Burl Ives"
+        band: "Gloria Branigan",
+        id: 6
     },
     {
-        band: "Childish Gambino"
+        band: "Burl Ives",
+        id: 7
     },
     {
-        band: "Elton John"
+        band: "Childish Gambino",
+        id: 8
     },
     {
-        band: "Crowded House"
+        band: "Elton John",
+        id: 9
+    },
+    {
+        band: "Crowded House",
+        id: 10
     }
 ];
 
 let selection = 0; // connect to button or something
 
 
-function Music (artists, albums, songs, selection){
+function Music (selection){
     this.musicInfo = {
         artist : artists[selection].band,
         album : albums[selection].album,
         song : songs[selection].song
     }
+    
 }
 
 Music.prototype = {
@@ -111,21 +144,65 @@ Music.prototype = {
     sayName : function(){
         console.log(this.musicInfo);
     },
-
     toString : function(){
         return artists[selection].band + " " + albums[selection].album + " " + songs[selection].song + " -- You did it!";
     }
-
 }
 
-var musicSelect1 = new Music(artists, albums, songs, selection);
-
-console.log(musicSelect1);
-musicSelect1.sayName(); //AHH It's works!!!! :-DDDDDDD omfg
-console.log(musicSelect1.toString());
-function InsertMusic(artists, albums, songs){
-
-    this.InsertMusic = {
-        
+function Collection(selection){
+    this.musicInfo = {
+        songs,
+        artists,
+        albums
     }
 }
+
+// Extends from Music
+Collection.prototype = new Music(selection);
+
+Collection.prototype.constructor = Collection;
+
+// Custom Prototypes
+Collection.prototype.push = function(band, album, song) { 
+    let id = artists.length+1;
+    artists.push({band, id}), 
+    id = albums.length+1;
+    albums.push({album, id}),
+    id = songs.length+1;
+    songs.push({song, id})
+    
+}
+
+Collection.prototype.returnID = function(ID) {
+    let _artist, _album, _song;
+    artists.forEach(function(item){
+        if(ID === item.id){
+            
+            _artist = item; 
+        }
+    });
+    albums.forEach(function(item){
+        if(ID === item.id){
+            
+            _album = item; 
+        }
+    });
+    songs.forEach(function(item){
+        if(ID === item.id){
+            
+            _song = item; 
+        }
+    });
+    return {_artist, _album, _song};
+}
+
+
+// Collection object which needs to be connected to HTML
+let musicCollection = new Collection(selection);
+console.log(musicCollection);
+
+musicCollection.sayName();
+musicCollection.push("Beyonce", "B'day", "Deja Vu");
+console.log(musicCollection.returnID(7));
+
+
